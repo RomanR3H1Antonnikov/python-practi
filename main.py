29de556faee83fbfb97d08285fbb96c6936829e2
Роман <roman.v.antonnikov@gmail.com>
2025-10-18 13:42:18 +0300

    Обновил структуру проекта

--- main.py
+++ main.py
@@ -4,42 +4,30 @@ try:
     print("Введите второе число:")
     second_num = int(input())
     result = first_num + second_num
-except ValueError:
-    print("Вы ввели нечисловое значение!")
-else:
     print("Результат сложения:", result, '\n')
 
-try:
     print("Введите первое число:")
     first_num = int(input())
     print("Введите второе число:")
     second_num = int(input())
     result = first_num - second_num
-except ValueError:
-    print("Вы ввели нечисловое значение!")
-else:
     print("Результат вычитания:", result, '\n')
 
-try:
     print("Введите первое число:")
     first_num = int(input())
     print("Введите второе число:")
     second_num = int(input())
     result = first_num * second_num
-except ValueError:
-    print("Вы ввели нечисловое значение!")
-else:
     print("Результат умножения:", result, '\n')
 
-try:
     print("Введите первое число:")
     first_num = int(input())
     print("Введите второе число:")
     second_num = int(input())
-    result = first_num / second_num
-except ZeroDivisionError:
-    print("На ноль делить нельзя!!")
+    try:
+        result = first_num / second_num
+        print("Результат деления:", result)
+    except ZeroDivisionError:
+        print("На ноль делить нельзя!!")
 except ValueError:
     print("Вы ввели нечисловое значение!")
-else:
-    print("Результат деления:", result)
