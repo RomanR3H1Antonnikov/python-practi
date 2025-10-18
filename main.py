f697dde5263c49d90bc0359deb45974b6e4b2935
Роман <roman.v.antonnikov@gmail.com>
2025-10-18 11:19:55 +0300

    Исправил структуру программы, опираясь на try/except

--- main.py
+++ main.py
@@ -1,30 +1,41 @@
-print("Введите первое число:")
-first_num = int(input())
-print("Введите второе число:")
-second_num = int(input())
-
-print("Результат сложения:", (first_num + second_num), '\n')
-
-print("Введите первое число:")
-first_num = int(input())
-print("Введите второе число:")
-second_num = int(input())
-
-print("Результат вычитания:", (first_num - second_num), '\n')
-
-print("Введите первое число:")
-first_num = int(input())
-print("Введите второе число:")
-second_num = int(input())
-
-print("Результат умножения:", (first_num * second_num), '\n')
-
-print("Введите первое число:")
-first_num = int(input())
-print("Введите второе число:")
-second_num = int(input())
+try:
+    print("Введите первое число:")
+    first_num = int(input())
+    print("Введите второе число:")
+    second_num = int(input())
+    result = first_num + second_num
+except ValueError:
+    print("Вы ввели нечисловое значение!")
+else:
+    print("Результат сложения:", result, '\n')
 
 try:
+    print("Введите первое число:")
+    first_num = int(input())
+    print("Введите второе число:")
+    second_num = int(input())
+    result = first_num - second_num
+except ValueError:
+    print("Вы ввели нечисловое значение!")
+else:
+    print("Результат вычитания:", result, '\n')
+
+try:
+    print("Введите первое число:")
+    first_num = int(input())
+    print("Введите второе число:")
+    second_num = int(input())
+    result = first_num * second_num
+except ValueError:
+    print("Вы ввели нечисловое значение!")
+else:
+    print("Результат умножения:", result, '\n')
+
+try:
+    print("Введите первое число:")
+    first_num = int(input())
+    print("Введите второе число:")
+    second_num = int(input())
     result = first_num / second_num
 except ZeroDivisionError:
     print("На ноль делить нельзя!!")
