b064fe785fab0887c667a6350f205cd67647e6ad
Роман <roman.v.antonnikov@gmail.com>
2025-10-20 13:18:47 +0300

    Контрольное исправление, перенёс всё в один try конструктор

--- main.py
+++ main.py
@@ -24,10 +24,10 @@ try:
     first_num = int(input())
     print("Введите второе число:")
     second_num = int(input())
-    try:
-        result = first_num / second_num
-        print("Результат деления:", result)
-    except ZeroDivisionError:
+    result = first_num / second_num
+    print("Результат деления:", result)
+
+except ZeroDivisionError:
         print("На ноль делить нельзя!!")
 except ValueError:
     print("Вы ввели нечисловое значение!")
