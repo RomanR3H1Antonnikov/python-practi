f5ca8b9098732d50d0556724900194a1390ec29b
Роман <roman.v.antonnikov@gmail.com>
2025-10-30 17:00:55 +0300

    Исправлены все ошибки

--- funct_pract.py
+++ funct_pract.py
@@ -1,11 +1,7 @@
 def max_number(a, b):
     if a > b:
         return a
-    elif a == b:
-        print("Числа должны быть различными!")
-        return None
-    else:
-        return b
+    return b
 
 
 def empty_function():
@@ -13,18 +9,18 @@ def empty_function():
 
 
 def even_numbers(n):
-    if n % 2 == 0 and n != 0:
-        yield n
-
+    for x in range(0, n + 1, 2):
+        yield x
 
 def test_max_number():
     assert max_number(1, 4) == 4, "Ошибка, 4 больше чем 1!"
     assert max_number(-4, 3) == 3, "Ошибка, 3 больше чем -4!"
-    assert max_number(2, 2) is None, "Ошибка, числы должны быть различными!"
+    assert max_number(2, 2) == 2, "Ошибка, числы должны быть различными!"
     print("Проверка успешно пройдена!")
 
 
-max_number(4, 7)
-empty_function()
-even_numbers(6)
+print(max_number(4, 10))
+print(empty_function())
+for num in even_numbers(6):
+    print(num)
 test_max_number()
