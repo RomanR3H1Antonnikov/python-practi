873ed9fc67b9cd9e96febf199c332c3f08f48c0d
Роман <roman.v.antonnikov@gmail.com>
2025-11-01 23:46:06 +0300

    Исправил текущие недочёты

--- condition.py
+++ condition.py
@@ -1,18 +1,16 @@
 def is_participation_approved(age, citizenship, is_disqualified):
-    if age < 18:
+    if age < 18 or citizenship == "Нет" or is_disqualified == "Да":
         return False
-    if citizenship == "Нет":
-        return False
-    elif citizenship != "Да":
+    elif citizenship != "Да" or is_disqualified:
         print("Нужно ответить 'Да' или 'Нет'!!")
-        return False
-    if is_disqualified == "Да":
         return False
     return True
 
 
-if is_participation_approved(int(input("Укажите ваш возраст: ")), input("Вы являетесь гражданином страны? Ответьте 'Да' или 'Нет': "),
-                             input("Вы не дисквалифицированы? Ответьте 'Да' или 'Нет': ")):
+age = int(input("Укажите ваш возраст: "))
+citizenship = input("Вы являетесь гражданином страны? Ответьте 'Да' или 'Нет': ")
+is_disqualified = input("Вы не дисквалифицированы? Ответьте 'Да' или 'Нет': ")
+if is_participation_approved(age, citizenship, is_disqualified):
     print("Поздравляю, вам можно голосовать на выборах!")
 else:
     print("К сожалению, вы не можете принимать участие в выборах")
