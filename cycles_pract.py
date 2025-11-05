abd66fb6332e4f90c492532d55b35450b8eea355
Роман <roman.v.antonnikov@gmail.com>
2025-11-05 13:06:49 +0300

    Сделал код более структурируемым и хорошо чистаемым

--- cycles_pract.py
+++ cycles_pract.py
@@ -1,13 +1,9 @@
-def get_cleaned_word(word):
-    return word.strip(',./?;:)(!@#$%^&*"\'')
-
-
-def count_vowels(word):
-    count = 0
-    for char in word:
+def count_vowels(x):
+    counter = 0
+    for char in x:
         if char in 'ёуеыаоэяию':
-            count += 1
-    return count
+            counter += 1
+    return counter
 
 
 def process_text(text):
@@ -18,29 +14,20 @@ def process_text(text):
     word_count_dict = {}
 
     for word in text.split():
-        cleaned_word = get_cleaned_word(word)
-
-        if not cleaned_word:
-            continue
-
+        word = word.strip(',./?;:)(!@#$%^&*"\'')
         word_counter += 1
-
-        if len(cleaned_word) > len(longest_word):
-            longest_word = cleaned_word
-
-        vowel_counter += count_vowels(cleaned_word)
-
-        if cleaned_word in word_count_dict:
-            word_count_dict[cleaned_word] += 1
+        if len(word) > len(longest_word):
+            longest_word = word
+        vowel_counter += count_vowels(word)
+        if word in word_count_dict:
+            word_count_dict[word] += 1
         else:
-            word_count_dict[cleaned_word] = 1
-
+            word_count_dict[word] = 1
     return word_count_dict, word_counter, longest_word, vowel_counter
 
 
 text = input("Введите или пришлите текст: ")
 word_count_dict, word_counter, longest_word, vowel_counter = process_text(text)
-
 print("Вот слова и количество раз, которое каждое слово встречается в тексте: ")
 for word, count in word_count_dict.items():
     print(f'{word}: {count}')
