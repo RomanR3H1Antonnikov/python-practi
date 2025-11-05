8bdd1c9ed9ca2189b8c87295dc830b3698991bf3
Роман <roman.v.antonnikov@gmail.com>
2025-11-05 19:25:18 +0300

    Реализовано задание Применение условий

--- cycles_pract.py
+++ cycles_pract.py
@@ -1,36 +1,24 @@
-def count_vowels(x):
+def sum_even_numbers():
     counter = 0
-    for char in x:
-        if char in 'ёуеыаоэяию':
-            counter += 1
+    for i in range(2, 101, 2):
+        counter += i
     return counter
 
 
-def process_text(text):
-    text = text.strip().lower()
-    longest_word = ''
-    word_counter = 0
-    vowel_counter = 0
-    word_count_dict = {}
+def square_odds():
+    return [n ** 2 for n in range(1, 11, 2)]
 
-    for word in text.split():
-        word = word.strip(',./?;:)(!@#$%^&*"\'')
-        word_counter += 1
-        if len(word) > len(longest_word):
-            longest_word = word
-        vowel_counter += count_vowels(word)
-        if word in word_count_dict:
-            word_count_dict[word] += 1
+
+print(sum_even_numbers(), square_odds())
+counter = 0
+print("Введите число:")
+try:
+    while True:
+        counter += 1
+        if int(input()) >= 0:
+            continue
         else:
-            word_count_dict[word] = 1
-    return word_count_dict, word_counter, longest_word, vowel_counter
-
-
-text = input("Введите или пришлите текст: ")
-word_count_dict, word_counter, longest_word, vowel_counter = process_text(text)
-print("Вот слова и количество раз, которое каждое слово встречается в тексте: ")
-for word, count in word_count_dict.items():
-    print(f'{word}: {count}')
-print("Количество слов в тексте:", word_counter)
-print("Самое длинное слово в тексте:", longest_word)
-print("Количество гласных букв:", vowel_counter)
+            break
+    print("Количество введённых чисел:", counter)
+except ValueError:
+    print("Вы ввели не число! Введите число!")
