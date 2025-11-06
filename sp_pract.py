e7c09d9b06673dd8bb6caece00709bd252aadc26
Роман <roman.v.antonnikov@gmail.com>
2025-11-06 20:36:50 +0300

    Применены текущие правки

--- sp_pract.py
+++ sp_pract.py
@@ -1,10 +1,26 @@
-sp1 = [1, 2, 3, 4, 5, 6, 7]
-sp2 = [1, 2, 3, 4]
-sp3 = []
-maxsp = max(sp1, sp2)
-for i in range(0, max(len(sp1), len(sp2))):
-    if i < len(min(sp1, sp2)):
-        sp3.append(max(sp1, sp2)[i] + min(sp1, sp2)[i])
+def max_list(list_1, list_2):
+    if len(list_1) >= len(list_2):
+        return list_1
     else:
-        sp3.append(maxsp[i])
-print(sp3)
+        return list_2
+
+
+def min_list(list_1, list_2):
+    if len(list_1) <= len(list_2):
+        return list_1
+    else:
+        return list_2
+
+
+list_1 = [1, 2, 3, 4, 5, 6, 7]
+list_2 = [1, 2, 3, 4]
+final_list = []
+maximal_list, minimal_list = max_list(list_1, list_2), min_list(list_1, list_2)
+
+
+for i in range(0, maximal_list):
+    if i < len(minimal_list):
+        final_list.append(maximal_list[i] + minimal_list[i])
+    else:
+        final_list.append(max_list(list_1, list_2)[i])
+print(final_list)
