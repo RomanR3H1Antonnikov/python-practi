b9fb3fcda934670d2fc85661069dbcbdad691c3e
Роман <roman.v.antonnikov@gmail.com>
2025-11-11 11:29:17 +0300

    Исправил задачу учитывая требования

--- sp_pract.py
+++ sp_pract.py
@@ -1,11 +1,7 @@
 def sum_lists(list_1, list_2):
     final_list = []
-    if len(list_1) >= len(list_2):
-        maximal_list = list_1
-        minimal_list = list_2
-    else:
-        maximal_list = list_2
-        minimal_list = list_1
+    maximal_list = max(list_1, list_2, key=len)
+    minimal_list = min(list_1, list_2, key=len)
 
     for i in range(len(maximal_list)):
         if i < len(minimal_list):
@@ -15,6 +11,6 @@ def sum_lists(list_1, list_2):
     return final_list
 
 
-list_1 = [1, 2, 3, 4, 5, 6, 7]
-list_2 = [1, 2, 3, 4]
+list_1 = [1, 200, 3, 4, 5, 6, 7]
+list_2 = [1, 2, 3, 400000]
 print(sum_lists(list_1, list_2))
