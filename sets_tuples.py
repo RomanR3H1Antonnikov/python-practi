a7878c7d8df3e789cedd0e82530ff3bece159433
Роман <roman.v.antonnikov@gmail.com>
2025-11-15 09:52:00 +0300

    Реализовано задание "Создание словаря библиотеки" 1

--- sets_tuples.py
+++ sets_tuples.py
@@ -1,12 +1,10 @@
-list_1 = []
-print("Вводите числа, заполняя список, введите -2, чтобы получить количество уникальных элементов в списке:")
-try:
-    while True:
-        current_num = int(input())
-        if current_num != -1:
-            list_1.append(current_num)
-        else:
-            break
-except ValueError:
-    print("Вводите числовые значения!")
-print(f"""Количество уникальных элементов в списке: {len(set(list_1))}""")
+library = {"book_name": {"author": "", "publishing_year": "", "availability": ""}}
+
+def book_list_view(library):
+    if library["book_name"]["author"] and library["book_name"]["publishing_year"] and library["book_name"]["availability"]:
+        print(library["book_name"])
+    else:
+        print("В библиотеке нет книг.")
+
+
+book_list_view(library)
