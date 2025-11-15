50f0af3f4500625714f41db1e338c61399ad40c1
Роман <roman.v.antonnikov@gmail.com>
2025-11-15 13:43:46 +0300

    Подправил задание 1

--- sets_tuples.py
+++ sets_tuples.py
@@ -1,13 +1,16 @@
 def book_list_view(library):
-    if all(library["book_name"].values()):
-        print(library["book_name"])
-    else:
+    found_books = False
+    for book_title, details in library.items():
+        if all(x != '' for x in details.values()):
+            print(f"""Книга: {book_title} Информация о книге: {details} """)
+            found_books = True
+    if not found_books:
         print("В библиотеке нет книг.")
 
 
 # def add_book(title, author, year):
 #     if title not in library:
-#         library["new_book_name"] = {
+#         library[title] = {
 #             "author": author,
 #             "publishing_year": year,
 #             "availability": None
@@ -16,7 +19,7 @@ def book_list_view(library):
 #     else:
 #         print("Книга с таким названием уже существует. Хотите обновить информацию о ней?")
 #         if input().lower() == "да":
-#             library["new_book_name"] = {
+#             library[title] = {
 #                 "author": author,
 #                 "publishing_year": year,
 #                 "availability": None
@@ -26,7 +29,14 @@ def book_list_view(library):
 #             pass
 
 
-library = {"book_name": {"author": "", "publishing_year": "", "availability": ""}}
+library = {
+    "Братья Карамазовы": {
+    "author": "Ф.М. Достоевский",
+    "publishing_year": 1879,
+    "availability": True
+}}
+book_list_view(library)
+# add_book('1984', 'J. Oruell', '1988')
 # book_list_view(library)
 # add_book('1984', 'J. Oruell', '1988')
 # book_list_view(library)
