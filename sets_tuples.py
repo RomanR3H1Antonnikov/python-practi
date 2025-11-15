4f282b4286034e0a3117f67d7d9251e057acf4ad
Роман <roman.v.antonnikov@gmail.com>
2025-11-15 11:29:58 +0300

    Поправил 1 задание из библиотеки

--- sets_tuples.py
+++ sets_tuples.py
@@ -1,10 +1,32 @@
-library = {"book_name": {"author": "", "publishing_year": "", "availability": ""}}
-
 def book_list_view(library):
-    if library["book_name"]["author"] and library["book_name"]["publishing_year"] and library["book_name"]["availability"]:
+    if all(library["book_name"].values()):
         print(library["book_name"])
     else:
         print("В библиотеке нет книг.")
 
 
-book_list_view(library)
+# def add_book(title, author, year):
+#     if title not in library:
+#         library["new_book_name"] = {
+#             "author": author,
+#             "publishing_year": year,
+#             "availability": None
+#         }
+#         print(f"""Книга "{title}" успешно добавлена!""")
+#     else:
+#         print("Книга с таким названием уже существует. Хотите обновить информацию о ней?")
+#         if input().lower() == "да":
+#             library["new_book_name"] = {
+#                 "author": author,
+#                 "publishing_year": year,
+#                 "availability": None
+#             }
+#             print(f"""Информация о книге {title} успешно обновлена!""")
+#         else:
+#             pass
+
+
+library = {"book_name": {"author": "", "publishing_year": "", "availability": ""}}
+# book_list_view(library)
+# add_book('1984', 'J. Oruell', '1988')
+# book_list_view(library)
