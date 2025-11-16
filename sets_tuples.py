c2629946b8781f9ef1ccc8570e23034ec31f2a30
Роман <roman.v.antonnikov@gmail.com>
2025-11-16 17:52:39 +0300

    Реализовано ДЗ: Добавление книги

--- sets_tuples.py
+++ sets_tuples.py
@@ -2,29 +2,30 @@ def book_list_view(library):
     if not library:
         print("В библиотеке нет книг.")
     else:
+        print("Доступные книги:")
         for book_title in library:
-            print(f"Доступные книги:\n{book_title}")
+            print(f"'{book_title}'")
 
 
-# def add_book(title, author, year):
-#     if title not in library:
-#         library[title] = {
-#             "author": author,
-#             "publishing_year": year,
-#             "availability": None
-#         }
-#         print(f"""Книга "{title}" успешно добавлена!""")
-#     else:
-#         print("Книга с таким названием уже существует. Хотите обновить информацию о ней?")
-#         if input().lower() == "да":
-#             library[title] = {
-#                 "author": author,
-#                 "publishing_year": year,
-#                 "availability": None
-#             }
-#             print(f"""Информация о книге {title} успешно обновлена!""")
-#         else:
-#             pass
+def add_book(title, author, year):
+    if title not in library:
+        library[title] = {
+            "author": author,
+            "publishing_year": year,
+            "availability": None
+        }
+        print(f"""Книга "{title}" успешно добавлена!""")
+    else:
+        print("Книга с таким названием уже существует. Хотите обновить информацию о ней? (Да/Нет)")
+        if input().lower() == "да":
+            library[title] = {
+                "author": author,
+                "publishing_year": year,
+                "availability": None
+            }
+            print(f"""Информация о книге {title} успешно обновлена!""")
+        else:
+            pass
 
 
 library = {
@@ -33,8 +34,9 @@ library = {
     "publishing_year": 1879,
     "availability": True
 }}
+
 book_list_view(library)
-# add_book('1984', 'J. Oruell', '1988')
-# book_list_view(library)
-# add_book('1984', 'J. Oruell', '1988')
-# book_list_view(library)
+add_book('1984', 'J. Oruell', '1988')
+book_list_view(library)
+add_book('1984', 'J. Oruell', '1988')
+book_list_view(library)
