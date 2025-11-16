d11d361057dba2cfae0ad7cc1142716601cbce2a
Роман <roman.v.antonnikov@gmail.com>
2025-11-16 20:51:47 +0300

    Исправления применены в 2 задании

--- sets_tuples.py
+++ sets_tuples.py
@@ -7,7 +7,7 @@ def book_list_view(library):
             print(f"'{book_title}'")
 
 
-def add_book(title, author, year):
+def add_book(library, title, author, year):
     if title not in library:
         library[title] = {
             "author": author,
@@ -25,7 +25,15 @@ def add_book(title, author, year):
             }
             print(f"""Информация о книге {title} успешно обновлена!""")
         else:
-            pass
+            print("Ваш ответ не 'Да', операция отменена")
+
+
+# def remove_book(title):
+#     if title in library:
+#         del library[title]
+#         print(f'Книга "{title}" успешно удалена из библиотеки')
+#     else:
+#         print("Такой книги нет в библиотеке!")
 
 
 library = {
@@ -36,7 +44,9 @@ library = {
 }}
 
 book_list_view(library)
-add_book('1984', 'J. Oruell', '1988')
+add_book(library, '1984', 'J. Oruell', '1988')
 book_list_view(library)
-add_book('1984', 'J. Oruell', '1988')
+add_book(library, '1984', 'J. Oruell', '1988')
 book_list_view(library)
+# remove_book("Братья Карамазовы")
+# book_list_view(library)
